af834fadc6d0ced924af4b3abefa9a27a7f6ec0e
Lucas Garcia <lucasugarcia@gmail.com>
2021-08-15 16:11:16 -0300

    Adição de interfaces para as classes Catalogo e Relatorio, permitindo o registro de um serviço de instânciação do Catalogo na classe de Startup, tirando essa a reponsabilidade dela e delegando isso para a injeção de dependência do ServiceProvider

--- Startup.cs
+++ Startup.cs
@@ -16,10 +16,11 @@ namespace web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICatalogo, Catalogo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -28,8 +29,8 @@ namespace web
 
             app.UseRouting();
 
-            Catalogo catalogo = new Catalogo();
-            Relatorio relatorio = new Relatorio(catalogo);
+            ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();
+            IRelatorio relatorio = new Relatorio(catalogo);
 
             app.UseEndpoints(endpoints =>
             {
