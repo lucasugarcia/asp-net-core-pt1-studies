21ec761a56d7dd76f3e9e48795ad87d5b6af2e9a
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-07 16:44:06 -0300

    Alterações para alterar a quantidade de um item no carrinho e recalcular o subtotal

--- CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -8,7 +8,7 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-        void UpdateQuantidade(ItemPedido itemPedido);
+        ItemPedido GetItemPedido(int itemPedidoId);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
@@ -17,16 +17,9 @@ namespace CasaDoCodigo.Repositories
         {
         }
 
-        public void UpdateQuantidade(ItemPedido itemPedido)
+        public ItemPedido GetItemPedido(int itemPedidoId)
         {
-            var itemPedidoDB = dbSet.Where(i => i.Id == itemPedido.Id).SingleOrDefault();
-
-            if(itemPedido != null)
-            {
-                itemPedidoDB.AtualizarQuantidade(itemPedido.Quantidade);
-
-                contexto.SaveChanges();
-            }
+            return dbSet.Where(i => i.Id == itemPedidoId).SingleOrDefault();
         }
     }
 }
